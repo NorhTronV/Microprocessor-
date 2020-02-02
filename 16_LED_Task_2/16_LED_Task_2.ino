2038836871fa730a09e02a55748eff9fd8879f64
Chantapol <60214656+NorhTronV@users.noreply.github.com>
2020-02-02 21:01:07 +0700

    16 LED Task 2

---
+++ 16_LED_Task_2/16_LED_Task_2.ino
@@ -0,0 +1,49 @@
+int clk_pin = 13; //SHCP
+int latch_pin = 10; //STCP
+int data_pin = 11; //DS
+
+void setup()
+{
+  pinMode(clk_pin, OUTPUT);
+  pinMode(latch_pin, OUTPUT);
+  pinMode(data_pin, OUTPUT);
+  pinMode(A0, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  for (int i = 0; i < 16 || i > 0; i = i)
+  {
+    digitalWrite(latch_pin, LOW);
+    for (int c = 0; c < 16; c++)
+    {
+      if (c == i)
+      {
+        digitalWrite(data_pin, HIGH);
+      }
+      else
+      {
+        digitalWrite(data_pin, LOW);
+      }
+      digitalWrite(clk_pin, HIGH);
+      digitalWrite(clk_pin, LOW);
+    }
+    Serial.println(analogRead(A1));
+    if (analogRead(A0) == 0)
+    {
+      i--;
+      if (i < 0) i = 15;
+      digitalWrite(latch_pin, HIGH);
+      delay(1000);
+    }
+    if (analogRead(A1) == 0)
+    {
+      i++;
+      if (i > 15) i = 0;
+      digitalWrite(latch_pin, HIGH);
+      delay(1000);
+    }
+    digitalWrite(latch_pin, HIGH);
+  }
+}
