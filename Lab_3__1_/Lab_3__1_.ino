d5d5f4f2397d2cfef36543f8a5c647730bb0befa
Chantapol <60214656+NorhTronV@users.noreply.github.com>
2020-02-06 14:52:21 +0700

    LAB 3

---
+++ Lab_3__1_/Lab_3__1_.ino
@@ -0,0 +1,41 @@
+#define DATA_PIN 11
+#define CLOCK_PIN 9
+#define LATCH_PIN 10
+
+int map7seg[10] = {
+  0x3f, //0
+  0x06, //1
+  0x5b, //2
+  0x4f, //3
+  0x66, //4
+  0x6d, //5
+  0x7d, //6
+  0x07, //7
+  0x7f, //8
+  0x6f, //9
+  
+};
+
+void setLed( uint8_t _state ){
+  digitalWrite(LATCH_PIN,LOW);
+  for(int i=0;i<8;i++){
+    digitalWrite(DATA_PIN,(_state>>(7-i))&0x01);
+    digitalWrite(CLOCK_PIN,HIGH);
+    digitalWrite(CLOCK_PIN,LOW);
+  }
+  digitalWrite(LATCH_PIN,HIGH);
+}
+void setup()
+{
+  pinMode(DATA_PIN,OUTPUT);
+  pinMode(CLOCK_PIN,OUTPUT);
+  pinMode(LATCH_PIN,OUTPUT);  
+}
+
+void loop()
+{
+  for(int i=0; i<10; i++){
+   setLed(map7seg[i]);
+    delay(500);
+  }
+}
